e3b475b0962a2d8a216b16d423303358ac9dd305
Felipe Kosouski <felipe.kosouski@gmail.com>
2020-01-07 23:15:43 -0300

    Added Domain Bus

---
+++ Domain/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+		protected Event()
+		{
+			Timestamp = DateTime.Now;
+		}
+    }
+}
